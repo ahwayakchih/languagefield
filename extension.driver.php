b941a1999a0545deb5a76329d134e41ee832457a
ahwayakchih <ahwayakchih@gmail.com>
2009-03-13 16:37:18 +0100

    Translation fix.

--- extension.driver.php
+++ extension.driver.php
@@ -2,13 +2,13 @@
 	Class extension_languageField extends Extension{
 	
 		public function about(){
-			return array('name' => 'Field: Language',
+			return array('name' => __('Field: Language'),
 						 'version' => '1.0',
 						 'release-date' => '2009-03-04',
 						 'author' => array('name' => 'Marcin Konicki',
 										   'website' => 'http://ahwayakchih.neoni.net',
 										   'email' => 'ahwayakchih@neoni.net'),
-						 'description' => 'Adds browser-languages parameter to list of all parameters available for data-sources and XSLT to use. Enables language field.'
+						 'description' => __('Adds browser-languages parameter to list of all parameters available for data-sources and XSLT to use. Enables language field.')
 			);
 		}
 
